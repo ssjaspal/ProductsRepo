c591b0979a089208cec6bb08cca67bb989f0ddb2
Shalini Singh Jaspal <shalinisingh.jaspal@.sheridancollege.ca>
2021-07-06 18:54:46 -0400

    Added product repository.

---
+++ ProductsApp/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp
+{
+    class ProductRepository
+    {
+        private List<Product> _products;
+
+        /// <summary>
+        /// Read-only property that return the collection of products in the repository
+        /// </summary>
+        public List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the collection of products
+        /// </summary>
+        public ProductRepository()
+        {
+            _products = new List<Product>();
+        }
+
+        /// <summary>
+        /// Adds a product to the repository.
+        /// </summary>
+        /// <param name="product">The new product to be added to the repository.</param>
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+        
+    }
+}
